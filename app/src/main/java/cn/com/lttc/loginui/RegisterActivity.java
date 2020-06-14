8f222119f5ae016735ecaffbb2a0f29a7fea5254
404Creater <653321360@qq.com>
2020-06-14 20:23:04 +0800

    first commit

---
+++ app/src/main/java/cn/com/lttc/loginui/RegisterActivity.java
@@ -0,0 +1,26 @@
+package cn.com.lttc.loginui;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_register_step_one);
+
+        findViewById(R.id.ib_navigation_back).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ib_navigation_back:
+                finish();
+                break;
+        }
+    }
+}
